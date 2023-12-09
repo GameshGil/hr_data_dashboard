9e6693c54386878dc3c9393cbff312c56b921dc7
Saeed <saeed-starik@mail.ru>
2023-12-09 05:34:38 +0300

    Добавлена обработка ошибки 404

--- backend/app.py
+++ backend/app.py
@@ -62,6 +62,11 @@ def login():
     return render_template('login.html', form=form)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
