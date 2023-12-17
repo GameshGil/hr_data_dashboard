de8235560bf632c5263593ac04de8ba685589cb1
Saeed <saeed-starik@mail.ru>
2023-12-17 21:10:36 +0300

    Доработанный docker-compose

--- backend/config.py
+++ backend/config.py
@@ -13,4 +13,13 @@ class Config:
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///project_db.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///project_db.db'
+
+    POSTGRES_USER = 'Saeed'
+    POSTGRES_PASSWORD = 'qwerty'
+    POSTGRES_DB = 'flask'
+    DB_HOST = 'flask-db'
+    DB_PORT = '5432'
+    SQLALCHEMY_DATABASE_URI = (
+        f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
+        f'@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}')
